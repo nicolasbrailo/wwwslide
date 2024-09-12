7ff982618cb10b559ba23c18424ff255bc84f61e
Nico B <nicolasbrailo@gmail.com>
2024-09-12 09:23:38 +0100

    Add cache cleanup

--- main.py
+++ main.py
@@ -142,6 +142,38 @@ def mk_image_hash(fpath):
 def mk_image_path_from_hash(hashedpath):
     return urllib.parse.unquote_plus(hashedpath)
 
+
+import threading
+import time
+import datetime
+def delete_old_files(directory, days_threshold):
+    try:
+        threshold_date = datetime.now() - timedelta(days=days_threshold)
+        files = os.listdir(directory)
+        for file in files:
+            file_path = os.path.join(directory, file)
+
+            if os.path.isfile(file_path):
+                last_modified_time = datetime.fromtimestamp(os.path.getmtime(file_path))
+
+                if last_modified_time < threshold_date:
+                    os.remove(file_path)
+                    print(f'Cleanup old cache {file_path}')
+
+    except Exception as e:
+       print('Failed to cleanup old cached assets')
+
+def setup_cleanup_cache(cache_dir):
+    def run():
+        while True:
+            delete_old_files(cache_dir, 1)
+            # Sleep for a day
+            time.sleep(60 * 60 * 24)
+    cleanup_cache_th = threading.Thread(target=run)
+    cleanup_cache_th.daemon = True  # Daemon thread will exit when the main program exits
+    cleanup_cache_th.start()
+
+
 class AlbumMgr:
     def __init__(self, conf):
         try:
@@ -211,6 +243,8 @@ with open("config.json", 'r') as fp:
 
 albums = AlbumMgr(CONF)
 app = Flask(__name__)
+
+setup_cleanup_cache(CONF["img_cache_directory"])
 
 @app.route('/')
 def index():
